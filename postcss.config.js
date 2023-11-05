8ee37afa327cc251fbfa0c3efa82631fff231ff5
virginus01 <info.vsolace@gmail.com>
2023-11-05 15:51:01 +0700

    update on tailwind and config file

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 // postcss.config.js
-export const plugins = {
-    tailwindcss: {},
-    autoprefixer: {},
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
