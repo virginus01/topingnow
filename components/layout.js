6be38c1ef838da808bc3e04ebcab70d2edb86552
virginus01 <info.vsolace@gmail.com>
2023-11-18 23:17:03 +0100

    added lang tag to html

--- components/layout.js
+++ components/layout.js
@@ -5,6 +5,7 @@ export default function Layout({ children }) {
     return (
         <>
             <Head>
+
                 <meta charSet="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link rel="icon" type="image/png" href="/favicon.ico" />
--- next.config.js
+++ next.config.js
@@ -9,4 +9,8 @@ module.exports = {
             },
         ],
     },
+    i18n: {
+        locales: ['en-US', 'fr', 'nl-NL'],
+        defaultLocale: 'en-US',
+    },
 }
