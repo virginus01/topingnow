85a9ba2286a924addad35d6ee02d9ce9ad055e22
solace <info.vsolace@gmail.com>
2024-01-27 07:24:07 +0100

    general

--- app/api/images/[path]/[id].png/route.tsx
+++ app/api/images/[path]/[id].png/route.tsx
@@ -1,25 +1,28 @@
 import { NextResponse } from "next/server";
-import puppeteer from "puppeteer";
-import chrome from "chrome-aws-lambda";
+//import puppeteer from "puppeteer";
+import chromium from "chrome-aws-lambda";
+import playwright from "playwright-core";
 
 export async function GET(
   request: any,
   { params }: { params: { path: string; id: string } }
 ) {
-  const browser = await puppeteer.launch({
-    headless: "new",
+  const browser = await playwright.chromium.launch({
+    args: chromium.args,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
   });
   const page = await browser.newPage();
   let res = NextResponse.next();
 
   const newHeaders = new Headers(request.headers);
 
-  await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 2 });
+  //await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 2 });
 
   await page.goto(
     `${process.env.NEXT_PUBLIC_BASE_URL}/gimages/${params.path}/${params.id}`,
     {
-      waitUntil: "networkidle0",
+      waitUntil: "load",
     }
   );
 
@@ -31,7 +34,7 @@ export async function GET(
 
   const buffer = await page.screenshot({
     omitBackground: true,
-    fullPage: true, //
+    fullPage: true,
   });
 
   await browser.close();
