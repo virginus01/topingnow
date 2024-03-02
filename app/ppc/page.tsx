68da8daceed4116cce46706d19c86acfb2696fca
virginus01 <info.vsolace@gmail.com>
2024-03-02 22:35:21 +0100

    bug fix

--- app/ppc/page.tsx
+++ app/ppc/page.tsx
@@ -2,25 +2,15 @@
 import React, { useEffect } from "react";
 import { redirect, useSearchParams } from "next/navigation";
 import { base_url } from "../utils/custom_helpers";
+import { useRouter } from "next/router";
 
 export default function Page() {
-  const searchParams = new URLSearchParams(window.location.search);
-  const url = searchParams ? searchParams.get("url") : base_url();
-  const utm_campaign = searchParams
-    ? searchParams.get("utm_campaign")
-    : "topingnow";
+  const router = useRouter();
+  const { url, utm_campaign } = router.query;
 
-  if (url) {
-    return redirect(
-      String(
-        `${url}?utm_source=${"topingnow.com"}&utm_medium=referral&utm_campaign=${utm_campaign}`
-      )
-    );
-  }
+  const redirectUrl = url
+    ? `${url}?utm_source=topingnow.com&utm_medium=referral&utm_campaign=${utm_campaign}`
+    : `${base_url()}?utm_source=topingnow.com&utm_medium=referral&utm_campaign=${utm_campaign}`;
 
-  return redirect(
-    String(
-      `${base_url()}?utm_source=${"topingnow.com"}&utm_medium=referral&utm_campaign=${utm_campaign}`
-    )
-  );
+  return redirect(redirectUrl);
 }
