acc805464c436f3738fb660bb556fb838821188d
virginus01 <info.vsolace@gmail.com>
2024-03-02 22:41:48 +0100

    bug fix

--- app/ppc/page.tsx
+++ app/ppc/page.tsx
@@ -2,15 +2,31 @@
 import React, { useEffect } from "react";
 import { redirect, useSearchParams } from "next/navigation";
 import { base_url } from "../utils/custom_helpers";
-import { useRouter } from "next/router";
 
 export default function Page() {
-  const router = useRouter();
-  const { url, utm_campaign } = router.query;
+  if (typeof window === "undefined") {
+    return null;
+  }
+  const searchParams = useSearchParams();
 
-  const redirectUrl = url
-    ? `${url}?utm_source=topingnow.com&utm_medium=referral&utm_campaign=${utm_campaign}`
-    : `${base_url()}?utm_source=topingnow.com&utm_medium=referral&utm_campaign=${utm_campaign}`;
+  if (searchParams) {
+    const url = searchParams ? searchParams.get("url") : base_url();
+    const utm_campaign = searchParams
+      ? searchParams.get("utm_campaign")
+      : "topingnow";
 
-  return redirect(redirectUrl);
+    if (url) {
+      return redirect(
+        String(
+          `${url}?utm_source=${"topingnow.com"}&utm_medium=referral&utm_campaign=${utm_campaign}`
+        )
+      );
+    }
+  }
+
+  return redirect(
+    String(
+      `${base_url()}?utm_source=${"topingnow.com"}&utm_medium=referral&utm_campaign=none`
+    )
+  );
 }
