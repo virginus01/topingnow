f918b6a5b0c1ba8b8857d5a05a9c976e6c237a28
virginus01 <info.vsolace@gmail.com>
2024-03-04 20:16:27 +0100

    image fix

--- app/api/images/[path]/[id].png/route.tsx
+++ app/api/images/[path]/[id].png/route.tsx
@@ -147,7 +147,53 @@ export async function GET(
       }
     );
   } catch (error) {
-    console.error("Error:", error);
-    return { success: false, msg: "Internal Server Error" };
+    return new ImageResponse(
+      (
+        <div
+          tw="w-full"
+          style={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundImage: `url(${base_images_url("beams-with.png")})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div tw="flex h-screen flex-col">
+            <div tw="flex justify-center text-center font-extrabold text-6xl text-blue-900 m-5">
+              Coming soon
+            </div>
+            <div tw="flex border-t-4 border-red-500"></div>
+
+            <div tw="flex justify-center text-center items-center font-extrabold text-3xl text-blue-900 m-1">
+              <div tw="flex">coming</div>
+              <img
+                src={base_images_url("top_1.png")}
+                alt=""
+                width={100}
+                height={100}
+                tw="flex rounded-sm object-cover"
+              />
+            </div>
+            <div tw="flex justify-center text-center items-center m-1">
+              <img
+                src={base_images_url("arrow_point.png")}
+                alt=""
+                width={60}
+                height={60}
+                tw="flex rounded-sm object-cover"
+              />
+            </div>
+            <div tw="flex items-end flex-grow">coming now</div>
+          </div>
+        </div>
+      ),
+      {
+        width: 1920,
+        height: 1080,
+      }
+    );
   }
 }
