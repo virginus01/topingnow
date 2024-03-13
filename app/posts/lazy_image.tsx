42f9ae01dad7ac9cefeaf0e36335404bea188d39
virginus01 <info.vsolace@gmail.com>
2024-03-13 15:04:21 +0100

    general

--- app/posts/lazy_image.tsx
+++ app/posts/lazy_image.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
 import { base_images_url, modifyImageUrl } from "../utils/custom_helpers";
 
-export default function image({ post, title, topicData, h = 500, w = 200 }) {
+export default function image({
+  post,
+  title,
+  topicData,
+  h = 500,
+  w = 200,
+  twh = "h-48",
+}) {
   const image = modifyImageUrl(post.processedImage);
   return (
     <>
@@ -13,7 +20,7 @@ export default function image({ post, title, topicData, h = 500, w = 200 }) {
           //style={{ width: "100%", height: "100%" }}
           width={h}
           height={w}
-          className="h-48 w-full rounded-sm object-cover"
+          className={`${twh} w-full rounded-sm object-cover`}
           blurDataURL={base_images_url("beams-with.png")}
           placeholder="blur"
           loading="lazy"
--- app/posts/post_body.tsx
+++ app/posts/post_body.tsx
@@ -30,7 +30,14 @@ export default function PostBody({ post }) {
   return (
     <div className={`bg-white px-2 py-2 my-5 rounded-sm`}>
       {!isNull(post.processedImage) && (
-        <LazyImage post={post} topicData={post} title={title} h={400} w={500} />
+        <LazyImage
+          post={post}
+          topicData={post}
+          title={title}
+          h={400}
+          w={500}
+          twh="w-full"
+        />
       )}
 
       <div className="mb-8">
