4b9fe6e0d3d5f4b4b234815aaea699fbbe69c7da
virginus01 <info.vsolace@gmail.com>
2024-03-30 13:26:42 +0100

    tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
+
     ],
 
     theme: {
         extend: {},
     },
     plugins: [
-        require('@tailwindcss/typography'),
-
+        require('@tailwindcss/typography')
     ],
 };
