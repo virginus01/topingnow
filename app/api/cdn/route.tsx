7a6ddd5b10c4a0f822a45b849960649a3d166ab7
virginus01 <info.vsolace@gmail.com>
2024-04-02 05:16:36 +0100

    bug fix

--- app/api/cdn/route.tsx
+++ app/api/cdn/route.tsx
@@ -2,7 +2,7 @@ import { revalidate } from "@/app/page";
 import axios from "axios";
 import sharp from "sharp";
 
-export const runtime = "edge";
+//export const runtime = "edge";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
